88f6908902fbae2be692b98dfcbc7a87fc3cd6ab
Srdjan S <stankovic.srdjo@gmail.com>
2020-02-10 16:56:04 +0100

    Fix event propagation

--- src/Cell.js
+++ src/Cell.js
@@ -1,16 +1,18 @@
 import React, {useState} from "react";
 import Timer from "./Timer";
+import GameContext from './contexts/GameContext';
 
-const Cell = ({ index, onClickHook, value, winningCell, resetGameHook }) => {
+const Cell = ({ index, onClickHook, value, winningCell }) => {
 
-  const [resetedTimer, setResetedTimer] = useState(false);
+  const {resetGame} = React.useContext(GameContext)
+  const [resetedTimer, setResetedTimer] = useState(false)
 
-  const resetButton = (index === 4) ? <button className="reset-button" onClick={resetGameHook}>Reset Game</button> :  null
+  const resetButton = (index === 4) ? <button className="reset-button" onClick={event => {event.stopPropagation(); resetGame()}}>Reset Game</button> :  null
   const timer = (index === 4) ? <Timer reseted={resetedTimer} setReseted={setResetedTimer}/> : null
 
   return (
     <div className={`cell${value ? " taken" : ""}${winningCell ? " won" : ""}`}>
-      <div className="inner-cell" onClick={_ => onClickHook(index)}>
+      <div className="inner-cell" onClick={_ => {onClickHook(index)}}>
       {resetButton}
         <p>{value}</p>
       {timer}
